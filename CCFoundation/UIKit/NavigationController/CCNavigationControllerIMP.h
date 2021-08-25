65f66f6f7f498fabfcdbd202f5425a5fd373142d
周金华 <zhoujinhua_xm@139.com>
2021-08-25 15:27:21 +0800

    1.修改bug
    1.修改bug

--- CCFoundation/UIKit/NavigationController/CCNavigationControllerIMP.h
+++ CCFoundation/UIKit/NavigationController/CCNavigationControllerIMP.h
@@ -10,7 +10,7 @@
 #import "CCNavigationProtocol.h"
 NS_ASSUME_NONNULL_BEGIN
 
-@interface CCNavigationControllerIMP : NSObject
+@interface CCNavigationControllerIMP : NSObject<UINavigationControllerDelegate>
 
 @end
 
